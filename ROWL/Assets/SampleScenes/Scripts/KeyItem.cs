7b023cee733a9d398ae38dffab9bfce9e3f7f1f8
TakakazuIkeda <ikeda.h.academy@gmail.com>
2021-08-22 12:23:39 +0900

    アイテムを追加設置、エンディングの分岐を実装しました

--- ROWL/Assets/SampleScenes/Scripts/KeyItem.cs
+++ ROWL/Assets/SampleScenes/Scripts/KeyItem.cs
@@ -7,9 +7,30 @@ public class KeyItem : MonoBehaviour
     public Dialogue Dialogue;
 
     public GameFlagDefine.GameFlag GameFlag;
-    
+
+    private ParticleSystem itemParticleSystem;
+
+    private void Start()
+    {
+        itemParticleSystem = GetComponentInChildren<ParticleSystem>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        itemParticleSystem.Stop();
+
+        if (GameFlag == GameFlagDefine.GameFlag.ManKeyItemGet)
+        {
+            if (other.gameObject.tag != "BoyCharacter")
+            {
+                return;
+            }
+            // 協力ポイントをセットする
+            CooperationJudge.Instance.SetCooperationPoint(CooperationParam.CooperationIndex.ManKeyItemGet);
+            Dialogue.DisplayDialogue("男らしいことができた");
+            return;
+        }
+
         if (!GameFlagDefine.GetGameFlag(GameFlag))
         {
             GameFlagDefine.SetGameFlag(GameFlag);
